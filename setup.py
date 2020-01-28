205163ab13ce374a289452280176140805b0acba
armanmohammadi704 <59266965+armanmohammadi704@users.noreply.github.com>
2020-01-28 19:46:01 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name='keras_resnet',
+    name='keras-resnet',
     version='0.2.2',
     description='Keras implementation of MaskRCNN instance aware segmentation.',
-    url='https://github.com/armanmohammadi704/keras_resnet',
+    url='https://github.com/armanmohammadi704/keras-resnet',
     maintainer='Hans Gaiser',
     maintainer_email='h.gaiser@fizyr.com',
     packages=setuptools.find_packages(),
-    install_requires=['keras', 'keras_resnet']
+    install_requires=['keras', 'keras-resnet']
 )
