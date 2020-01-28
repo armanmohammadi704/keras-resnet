904a6885ff91160d2d06652b5afea67209dccd6a
armanmohammadi704 <59266965+armanmohammadi704@users.noreply.github.com>
2020-01-28 20:07:16 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='keras-resnet',
-    version='0.2.2',
+    version='0.1.0',
     description='Keras implementation of MaskRCNN instance aware segmentation.',
     url='https://github.com/armanmohammadi704/keras-resnet',
     maintainer='Hans Gaiser',
