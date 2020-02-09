e8258b94dc8465d048bac6a7e19f262f42c781a0
armanmohammadi704 <59266965+armanmohammadi704@users.noreply.github.com>
2020-02-09 14:58:07 +0330

    Update _2d.py

--- keras_resnet/models/_2d.py
+++ keras_resnet/models/_2d.py
@@ -73,7 +73,7 @@ class ResNet2D(keras.Model):
         if numerical_names is None:
             numerical_names = [True] * len(blocks)
 
-        x = keras.layers.Conv3D(64, (7, 7,3), padding='same', use_bias=False, name="conv1", padding="same")(inputs)
+        x = keras.layers.Conv3D(64, (7, 7,3), use_bias=False, name="conv1", padding="same")(inputs)
         #x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
         x = keras.layers.Activation("relu", name="conv1_relu")(x) 
         x=keras.layers.Reshape((-1,1067,64))(x)
