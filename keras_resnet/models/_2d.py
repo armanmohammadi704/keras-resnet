8b3f9933d92d93f7302ae7e020c048a9b3135492
armanmohammadi704 <59266965+armanmohammadi704@users.noreply.github.com>
2020-02-16 00:48:41 +0330

    Update _2d.py

--- keras_resnet/models/_2d.py
+++ keras_resnet/models/_2d.py
@@ -76,6 +76,7 @@ class ResNet2D(keras.Model):
         x = keras.layers.Conv3D(64, (7, 7,3), strides=(2, 2,1), use_bias=False, name="conv1", padding="same")(inputs)
         x = keras_resnet.layers.BatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
         x = keras.layers.Activation("relu", name="conv1_relu")(x)
+        x = keras.layers.Conv3D(64, (7, 7,3), use_bias=False, name="conv1", padding="same")(inputs)
         x = keras.layers.Conv3D(1, (7, 7,3), use_bias=False, name="conv1", padding="same")(inputs)
         x = keras.layers.Reshape((400,-1,9))(x)
         x = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool1")(x)
